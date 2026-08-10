03c751842139245d1a374ec1e80adb3ba1234a67
fihmandbarmu <care4fihmchangeh@gmail.com>
2026-08-10 21:37:31 +0100

    Create messageTemplates for birthday greetings
    Added various birthday message templates for friends, family, and colleagues.

---
+++ messages.js
@@ -0,0 +1,18 @@
+// messages.js
+const messageTemplates = [
+    { label: "-- Choose an inspiration --", text: "" },
+    
+    // Friends
+    { label: "Friend - Heartfelt", text: "Happy birthday to my amazing friend! Wishing you a day filled with love, laughter, and everything you desire. Thank you for always being there." },
+    { label: "Friend - Short & Sweet", text: "Wishing you the happiest of birthdays and a fantastic year ahead! Cheers to you! 🥂" },
+    { label: "Friend - Funny", text: "Happy birthday! I was going to make a joke about getting older, but I remembered we do the same stupid things we did 10 years ago. Have a great one!" },
+    
+    // Family
+    { label: "Family - Parent", text: "To the best parent in the world, happy birthday! Thank you for your endless love, patience, and support. I hope your day is as wonderful as you are." },
+    { label: "Family - Sibling", text: "Happy birthday! Growing up with you was the greatest adventure. Even though we drive each other crazy, I wouldn't trade you for the world." },
+    
+    // Colleagues
+    { label: "Colleague - Professional", text: "Wishing you a very happy birthday and a successful year ahead. It's a pleasure working with you!" },
+    { label: "Colleague - Casual", text: "Happy Birthday! Hope you get to take a break, eat lots of cake, and enjoy your special day away from the spreadsheets." },
+    
+   ];
